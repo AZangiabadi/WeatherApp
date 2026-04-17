61598ab29fdfe1992e59da5b957cd772e22608c0
AZangiabadi <aazangiabadi@gmail.com>
2026-04-17 01:01:41 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Amirali Zangiabadi on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
